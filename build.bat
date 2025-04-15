0f0573dd87306aaee4b5953ed04b06d811e87749
0x7030676e31 <0x7030676e31@proton.me>
2025-04-15 14:17:29 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,15 @@
 @echo off
 
 cargo build --release
-move target\release\hephaestus.dll hephaestus.dll
-python3 setup.py build_ext --inplace
+python setup.py build_ext --inplace
 
+copy /Y target\release\hephaestus.dll execution\hephaestus.dll
+copy /Y injector.cp313-win_amd64.pyd execution\injector.cp313-win_amd64.pyd
+
+del target\release\hephaestus.dll
+del injector.cp313-win_amd64.pyd
+
+echo:
 echo ===========================================================
 echo To inject the Rust DLL, launch the 'main.py' python script.
 echo ===========================================================
